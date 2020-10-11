7aee53224c76c309247c44075b2e4ec801f7906f
Vincent John Cagay <me@vjcagay.com>
2020-10-11 21:50:50 +0900

    :bug: Fix click away save bug

--- frontend/src/Todo.jsx
+++ frontend/src/Todo.jsx
@@ -7,18 +7,21 @@ const Todo = ({ value, onChange, onDelete }) => {
 
   useEffect(() => {
     if (edit) {
+      // Copy the value from parent first as initial value
+      setTask(value.task);
       input.current.focus();
       document.addEventListener("click", isOutSideClick);
+    } else {
+      // When saving make sure it's not empty
+      if (task.trim()) {
+        onChange({ ...value, task: task.trim() });
+      }
     }
 
     return () => document.removeEventListener("click", isOutSideClick);
   }, [edit]);
 
-  const isOutSideClick = (event) => {
-    if (event.target !== input.current) {
-      setEdit(false);
-    }
-  };
+  const isOutSideClick = (event) => event.target !== input.current && setEdit(false);
 
   return (
     <li className="group border-t px-4 py-2 text-gray-700 flex items-center" onDoubleClick={() => setEdit(true)}>
@@ -28,12 +31,7 @@ const Todo = ({ value, onChange, onDelete }) => {
           className="w-full py-1 outline-none"
           value={task}
           onChange={(event) => setTask(event.target.value)}
-          onKeyUp={(event) => {
-            if (event.key === "Enter" && task.trim()) {
-              setEdit(false);
-              onChange({ ...value, task: task.trim() });
-            }
-          }}
+          onKeyUp={(event) => event.key === "Enter" && task.trim() && setEdit(false)}
           ref={input}
         />
       ) : (
