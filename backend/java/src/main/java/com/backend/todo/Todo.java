4679ac0a4f0234d2d3d6304231db43f9adbac629
Vincent John Cagay <me@vjcagay.com>
2020-10-17 16:32:36 +0900

    :sparkles: Setup Java Spring

---
+++ backend/java/src/main/java/com/backend/todo/Todo.java
@@ -0,0 +1,13 @@
+package com.backend.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Todo.class, args);
+	}
+
+}
