d8c19b7c230bfb4f9c276b6f463f8939929e9730
Vincent John Cagay <me@vjcagay.com>
2020-10-17 20:14:59 +0900

    :sparkles: Implement PUT to Spring

--- backend/java/src/main/java/com/backend/todo/TodoService.java
+++ backend/java/src/main/java/com/backend/todo/TodoService.java
@@ -4,8 +4,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,4 +38,9 @@ public class TodoService {
     return todo;
   }
 
+  @PutMapping("/todos")
+  public Todo updateTodo(@RequestBody Todo todo) {
+    repository.save(todo);
+    return todo;
+  }
 }
