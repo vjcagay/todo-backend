2b240914563f93f675fb35d12285ee5898f47c25
Vincent John Cagay <me@vjcagay.com>
2020-11-03 12:13:39 +0900

    :wrench: Update endpoints for java

--- backend/java/src/main/java/com/backend/todo/TodoRepository.java
+++ backend/java/src/main/java/com/backend/todo/TodoRepository.java
@@ -1,6 +1,7 @@
 package com.backend.todo;
 
 import java.util.List;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -8,5 +9,9 @@ public interface TodoRepository extends MongoRepository<Todo, String> {
 
   // We write a custom delete operation based on the filter
   @Query(value = "{ done: true }", delete = true)
-  public List<Todo> deleteDoneTodos();
+  public List<Todo> deleteDone();
+
+  // Map _id as a query by using ?0
+  @Query(value = "{ '_id': ?0 }", delete = true)
+  public List<Todo> deleteOne(String _id);
 }
--- backend/java/src/main/java/com/backend/todo/TodoService.java
+++ backend/java/src/main/java/com/backend/todo/TodoService.java
@@ -1,11 +1,14 @@
 package com.backend.todo;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,11 +27,13 @@ public class TodoService {
 
   // "/" automatically maps to resources/static
 
+  // Returns an array of Todos
   @GetMapping("/todos")
   public List<Todo> getTodos() {
     return repository.findAll();
   }
 
+  // Returns new Todo with the generated id
   @PostMapping("/todos")
   public Todo addTodo(@RequestBody Todo todo) {
     // _id will be assigned to todo after successful insertion
@@ -38,20 +43,30 @@ public class TodoService {
     return todo;
   }
 
-  @PutMapping("/todos")
-  public Todo updateTodo(@RequestBody Todo todo) {
-    repository.save(todo);
-    return todo;
+  // Returns Todo with the updated values
+  @PutMapping("/todos/{_id}")
+  public Todo updateTodo(@PathVariable(name = "_id") String _id, @RequestBody Todo todo) {
+    System.out.println(_id);
+    Todo updatedTodo = new Todo(_id, todo.getTask(), todo.getDone());
+    repository.save(updatedTodo);
+    return updatedTodo;
   }
 
+  // Returns { deleted: 1 }
+  @DeleteMapping("/todos/{_id}")
+  public Map deleteTodo(@PathVariable(name = "_id") String _id) {
+    List<Todo> deletedTodo = repository.deleteOne(_id);
+
+    // Along with Map, it this endpoin to return a custom JSON object
+    return Collections.singletonMap("deleted", deletedTodo.size());
+  }
+
+  // Returns { deleted: <no. of deleted todos }
   @DeleteMapping("/todos")
-  public Todo deleteTodo(@RequestBody Todo todo) {
-    repository.delete(todo);
-    return todo;
-  }
+  public Map<String, Integer> deleteAllDone() {
+    List<Todo> deletedTodos = repository.deleteDone();
 
-  @DeleteMapping("/todos/all")
-  public List<Todo> deleteAllDone() {
-    return repository.deleteDoneTodos();
+    // Along with Map, it this endpoin to return a custom JSON object
+    return Collections.singletonMap("deleted", deletedTodos.size());
   }
 }
